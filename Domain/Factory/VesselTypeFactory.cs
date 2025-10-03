e4c96e0df0f2549639dc1745b7af213a3591e4bb
Ruben Freitas <1231267@isep.ipp.pt>
2025-10-03 19:37:21 +0100

    Little fix on Vessel Type Factory

--- Domain/Factory/VesselTypeFactory.cs
+++ Domain/Factory/VesselTypeFactory.cs
@@ -5,7 +5,7 @@ using ShippingManagement.Domain.Vessels;
 
 public class VesselTypeFactory : IVesselTypeFactory
 {
-    public VesselType newVesselType(string name, string description, int capacity, int maxRows, int maxBays, int maxTiers)
+    public VesselType NewVesselType(string name, string description, int capacity, int maxRows, int maxBays, int maxTiers)
     {
         return new VesselType(name, description, capacity, maxRows, maxBays, maxTiers);
     }
