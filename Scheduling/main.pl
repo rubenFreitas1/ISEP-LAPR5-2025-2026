0591cfdea868697b01ae33f38e684b7a43dc6553
Ruben Freitas <1231267@isep.ipp.pt>
2025-11-16 21:26:31 +0000

    another change to main.pl

--- Scheduling/main.pl
+++ Scheduling/main.pl
@@ -3,8 +3,14 @@
 :- use_module(library(http/http_json)).
 :- consult('http_handler.pl').
 
-:- initialization(start_server).
+:- initialization(start).
 
+start :-
+    start_server,
+    wait_forever.
 
 start_server :-
-    http_server(http_dispatch, [port(6000), reuse_addr(true)] ).
+    http_server(http_dispatch, [port(6000), reuse_addr(true)]).
+
+wait_forever :-
+    thread_get_message(_).
